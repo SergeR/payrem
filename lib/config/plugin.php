ab22b16a40e9f500aafb3c85a1207978be56c0f0
Serge Rodovnichenko <sergerod@gmail.com>
2015-02-01 01:09:39 +0300

    Initial commit

---
+++ lib/config/plugin.php
@@ -0,0 +1,10 @@
+<?php
+return array (
+  'name' => 'Payment Reminder',
+  'icon' => 'img/payrem.gif',
+  'version' => '1.0.0',
+  'vendor' => '670917',
+  'handlers' => 
+  array (
+  ),
+);
---
+++ lib/shopPayremPlugin.class.php
@@ -0,0 +1,6 @@
+<?php
+
+class shopPayremPlugin extends shopPlugin
+{
+
+}
