04995af4029661ec25836d5c1821123db21fffa8
Serge Rodovnichenko <sergerod@gmail.com>
2015-02-09 04:51:06 +0300

    Вызов своего контроллера настроек

--- lib/config/plugin.php
+++ lib/config/plugin.php
@@ -7,8 +7,9 @@
  * @license http://www.webasyst.com/terms/#eula Webasyst
  */
 return array(
-    'name'    => _wp('Payment Reminder'),
-    'img'     => 'img/pay_remind_icon_16.png',
-    'version' => '1.0.0',
-    'vendor'  => '670917'
+    'name'          => _wp('Payment Reminder'),
+    'img'           => 'img/pay_remind_icon_16.png',
+    'version'       => '1.0.0',
+    'vendor'        => '670917',
+    'shop_settings' => TRUE
 );
